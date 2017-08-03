0ead1f274dceff065f1d81e6e41c4ccec3f15e1f
alexandervonkurtz <alexander.vonkurtz@my.jcu.edu.au>
2017-08-04 01:08:28 +1000

    Add files via upload

---
+++ TableAndChairs/src/TableAndChairs.java
@@ -0,0 +1,12 @@
+public class TableAndChairs {
+    // can just use spaces but displayed various tabbed methods and spaces
+
+    public static void main(String[] args){
+        System.out.println("X" + "\t \t \t \t \t \t" + "X");
+        System.out.println("X" + "\t \t \t \t \t \t" + "X");
+        System.out.println("X" + "\t \t" + "XXXXXXXXXX" + "\t \t" + "X");
+        System.out.println("XXXXXX" + "  " + "X" + "\t \t" + " " + "X" + "  " + "XXXXX");
+        System.out.println("X" + "\t" + " X" + "  " + "X" + "\t \t" + " X" + "  " + "X \t" + "X");
+        System.out.println("X" + "\t" + " X" + "  " + "X" + "\t \t" + " X" + "  " + "X \t" + "X");
+    }
+}
